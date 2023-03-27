aee60d39dbbb9924b1de2fae4e088ecab4f790c7
Marek Pałdyna <marekpaldyna@wp.pl>
2023-03-27 14:53:48 +0200

    new home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -47,34 +47,65 @@ class HomePageScreen extends StatelessWidget {
       body: Consumer<RecipesProvider>(
         builder: (context, recipesProvider, child) {
           var recipes = recipesProvider.recipes;
-          return ListView.builder(
-            itemCount: recipes.length,
-            itemBuilder: (context, index) {
-              String tags = "Tags: ";
-              for (var tag in recipes[index].tags) {
-                tags += "${tag.name}, ";
-              }
-              tags = tags.substring(0, tags.length - 2);
-              return ListTile(
-                title: Text(recipes[index].title),
-                subtitle: Text(
-                  tags,
-                  overflow: TextOverflow.ellipsis,
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount:
+                      MediaQuery.of(context).orientation == Orientation.portrait
+                          ? 2
+                          : 3,
                 ),
-                leading: recipes[index].image == null
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(Icons.image),
+                itemCount: recipes.length,
+                itemBuilder: (context, index) {
+                  String tags = "Tags: ";
+                  for (var tag in recipes[index].tags) {
+                    tags += "${tag.name}, ";
+                  }
+                  tags = tags.substring(0, tags.length - 2);
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: recipes[index].image == null
+                                ? const Icon(Icons.image)
+                                : ClipRRect(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    child: Image.file(
+                                      recipes[index].image!,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                          ),
+                          Text(
+                            recipes[index].title,
+                            overflow: TextOverflow.fade,
+                            maxLines: 1,
+                            softWrap: false,
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          Text(
+                            tags,
+                            overflow: TextOverflow.ellipsis,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          // TextButton(
+                          //   onPressed: () {
+                          //     Navigator.of(context).pushNamed(
+                          //       RecipeDetailScreen.routeName,
+                          //       arguments: recipes[index].id,
+                          //     );
+                          //   },
+                          //   child: const Text('View Recipe'),
+                          // ),
                         ],
-                      )
-                    : Image.file(recipes[index].image!),
-                onTap: () {
-                  Navigator.of(context).pushNamed(RecipeDetailScreen.routeName,
-                      arguments: recipes[index].id);
-                },
-              );
-            },
+                      ),
+                    ),
+                  );
+                }),
           );
         },
       ),
